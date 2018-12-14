166cc4f6363570841254d2d3cd4b2224f5d1725d
Kaydax <kaydax@kaydax.xyz>
2018-12-14 10:13:13 -0500

    Slight fix for dbots

--- utils/Discord-Bots-api.js
+++ utils/Discord-Bots-api.js
@@ -6,26 +6,22 @@ module.exports = DBA;
 DBA.postGuilds = async function(bot, token)
 {
   var url = "https://discord.bots.gg/api/v1/bots/316520238835433482/stats";
-  for(var i = 0; i < bot.shards.size; i++)
+  var options =
   {
-    var options =
-    {
-      method: 'POST',
-      uri: url,
-      body: {
-        "shard_id": i,
-        "shard_count": bot.shards.size,
-        "server_count": bot.guilds.size
-      },
-      headers: {
-        "Authorization": token
-      },
-      json: true
-    };
-    rp(options).catch(function (err) {
-      console.log(err);
-    });
-  }
+    method: 'POST',
+    uri: url,
+    body: {
+      "shard_count": bot.shards.size,
+      "server_count": bot.guilds.size
+    },
+    headers: {
+      "Authorization": token
+    },
+    json: true
+  };
+  rp(options).catch(function (err) {
+    console.log(err);
+  });
 
   console.log("DBots Server Count Posted!");
 }
